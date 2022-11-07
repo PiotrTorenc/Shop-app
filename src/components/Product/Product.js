6fdea8e13c062dd08e1d82d8880e7c45944b25c4
PiotrTorenc <piotr.torenc@gmail.com>
2022-11-08 00:09:48 +0100

    add useMemo

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,6 +1,6 @@
 import styles from './Product.module.scss';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import ProductImage from './ProductImage/ProductImage';
 import ProductForm from './ProductForm/ProducForm';
 
@@ -8,10 +8,10 @@ const Product = ({ name, title, colors, sizes, basePrice }) => {
   const [currentColor, setCurrentColor] = useState(colors[0]);
   const [currentSize, setCurrentSize] = useState(sizes[0].name);
 
-  const getPrice = () => {
+  const getPrice = useMemo(() => {
     const foundSize = sizes.find((element) => element.name === currentSize);
     return basePrice + foundSize.additionalPrice;
-  };
+  }, [basePrice, sizes, currentSize]);
 
   const addToCart = (event) => {
     event.preventDefault();
@@ -19,7 +19,7 @@ const Product = ({ name, title, colors, sizes, basePrice }) => {
     console.log('Summary');
     console.log('============');
     console.log('Name:', title);
-    console.log('Price:', getPrice());
+    console.log('Price:', getPrice);
     console.log('Size:', currentSize);
     console.log('Color:', currentColor);
   };
@@ -30,7 +30,7 @@ const Product = ({ name, title, colors, sizes, basePrice }) => {
       <div>
         <header>
           <h2 className={styles.name}>{title}</h2>
-          <span className={styles.price}>Price: {getPrice()}$</span>
+          <span className={styles.price}>Price: {getPrice}$</span>
         </header>
         <ProductForm
           addToCart={addToCart}
